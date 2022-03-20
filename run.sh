d088605a9839076aa3815c4eca35aec89f496277
Fabian Ruiz <fabian.ruiz@alumnos.uach.cl>
2022-03-20 18:42:15 -0300

    Changed directory with the use of pwd for run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker build . -t localfreeling
 docker kill freeling_container
 docker rm freeling_container
-docker run --volume=/home/fruiz/testing/freeling_files:/root/freeling_files --volume=/home/fruiz/testing/it.cfg:/usr/local/share/freeling/config/it.cfg --name freeling_container -d -i -t localfreeling:latest
+docker run --volume=$PWD/freeling_files:/root/freeling_files --volume=$PWD/it.cfg:/usr/local/share/freeling/config/it.cfg --name freeling_container -d -i -t localfreeling:latest
